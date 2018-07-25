0f19e20c2e97fe656dde5c4402a6b59c7de36d5d
Reavershark <jonasty1999@gmail.com>
2018-07-26 00:38:14 +0200

    Switcher

--- source/app.d
+++ source/app.d
@@ -3,8 +3,7 @@ import gtk.MainWindow;
 import gtk.Box;
 import gtk.Stack;
 import gtk.StackSwitcher;
-import gdk.Event;
-import gtk.Widget;
+import gtk.Label;
 
 private MainWindow mainWin;
 
@@ -13,50 +12,20 @@ void main(string[] args) {
 
     mainWin = new MainWindow("GtkRpg");
 
-    //Button buttonPlay = new Button("Play");
-    //Button buttonLoadout = new LoadoutButton("Loadout editor");
-    //ExitButton buttonExit = new ExitButton("Exit");
     Stack stack = new Stack();
     StackSwitcher stackSwitcher = new StackSwitcher();
     stackSwitcher.setStack(stack);
+    
+    stack.addTitled(new Label("Play menu"), "play", "Play");
+    stack.addTitled(new Label("Loadout editor"), "loadout", "Loadout");
+    stack.addTitled(new Label("Collection"), "collection", "Collection");
 
     Box box = new Box(Orientation.VERTICAL, 30);
-    //box.packStart(buttonPlay, true, true, 0);
-    //box.packStart(buttonLoadout, true, true, 0);
-    //box.packStart(buttonExit, true, true, 0);
+    box.packStart(stackSwitcher, true, true, 0);
+    box.packStart(stack, true, true, 0);
     
     mainWin.setDefaultSize(200, 200);
     mainWin.add(box);
     mainWin.showAll();
     Main.run;
 }
-
-//class LoadoutButton : Button {
-//
-//    this(in string text) {
-//        super(text);
-//        addOnButtonRelease(&openLoadout);
-//    }
-//
-//    private bool openLoadout(Event event, Widget widget) {
-//        MainWindow loadoutWin = new MainWindow("Loadout editor");
-//        loadoutWin.setDefaultSize(200, 200);
-//        loadoutWin.add(new Button("Loadout editor"));
-//        loadoutWin.showAll();
-//
-//        mainWin.close();
-//        return true;
-//    }
-//}
-//
-//class ExitButton : Button {
-//    this(in string text) {
-//        super(text);
-//        addOnButtonRelease(&exit);
-//    }
-//
-//    private bool exit(Event event, Widget widget) {
-//        Main.quit();
-//        return true;
-//    }
-//}
