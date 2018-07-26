393a414a637f2e17eeb5bfc4a54ff96f2a7df99c
Reavershark <jonasty1999@gmail.com>
2018-07-26 16:34:08 +0200

    Using glade

--- source/app.d
+++ source/app.d
@@ -1,31 +1,17 @@
+import gtk.Builder;
 import gtk.Main;
-import gtk.MainWindow;
-import gtk.Box;
-import gtk.Stack;
-import gtk.StackSwitcher;
-import gtk.Label;
-
-private MainWindow mainWin;
+import gtk.Widget;
+import gtk.Window;
+import std.stdio;
 
 void main(string[] args) {
     Main.init(args);
+    Builder builder = new Builder();
+    builder.addFromFile("app.glade");
 
-    mainWin = new MainWindow("GtkRpg");
+    Window win = cast(Window)builder.getObject("window");
+    win.addOnHide( delegate void(Widget aux){ Main.quit(); } );
 
-    Stack stack = new Stack();
-    StackSwitcher stackSwitcher = new StackSwitcher();
-    stackSwitcher.setStack(stack);
-    
-    stack.addTitled(new Label("Play menu"), "play", "Play");
-    stack.addTitled(new Label("Loadout editor"), "loadout", "Loadout");
-    stack.addTitled(new Label("Collection"), "collection", "Collection");
-
-    Box box = new Box(Orientation.VERTICAL, 30);
-    box.packStart(stackSwitcher, true, true, 0);
-    box.packStart(stack, true, true, 0);
-    
-    mainWin.setDefaultSize(200, 200);
-    mainWin.add(box);
-    mainWin.showAll();
+    win.showAll();
     Main.run;
 }
